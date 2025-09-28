eb149ec25f69eba4f3966a8de67532a9ab24a2e4
Hannah Sundar <rhythmsofjoy.org@gmail.com>
2025-09-28 22:31:24 +0000

    title

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 from db import DDB
 
 st.set_page_config(
-    page_title="NeuroTunes",
+    page_title="NeuroHarmony",
     page_icon="🎵",
     layout="wide",
     initial_sidebar_state="expanded"
