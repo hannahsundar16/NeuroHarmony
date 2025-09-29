62edb467adaa154d42c748f86fcab3e6dc2b7bcc
Hannah Sundar <rhythmsofjoy.org@gmail.com>
2025-09-29 14:54:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ def main():
     # Title and image
     st.title("NeuroHarmony")
    # st.image(IMAGE_ADDRESS, caption="EEG Frequency Bands (Delta, Theta, Alpha, Beta, Gamma)")
-    st.image("neuroharmony.png", caption="EEG Frequency Bands", width=300)
+    st.image("neuroharmony.png", caption="EEG Frequency Bands", width=500)
     st.markdown("---")
     # Authenticate
     user = get_user_simple()
