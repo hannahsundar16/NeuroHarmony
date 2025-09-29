5781afe7a4391d50a9918e36534bea7ca2ae74ff
Hannah Sundar <rhythmsofjoy.org@gmail.com>
2025-09-28 20:12:20 -0400

    Update db.py

--- db.py
+++ db.py
@@ -280,7 +280,7 @@ class DDB:
     @staticmethod
     def _default_audio_url(category: str) -> str:
         urls = {
-            "Classical": "https://archive.org/details/debussysoundreco00debu/01_Feux_d_artifice.mp3",
+            "Classical": "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3",
             "Rock": "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3",
             "Pop": "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3",
             "Rap": "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3",
